382af44376f7829ed54f3c7408b077b0605e290b
JosWambugu <josphatwambugu77@gmail.com>
2020-07-16 15:34:36 +0300

    Third Commit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,7 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import { applyMiddleware, compose, createStore } from "redux";
+
 import rootReducer from "./reducers";
+import thunk from "redux-thunk";
 
 const initialState = {};
 
@@ -11,7 +12,7 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+   
   )
 );
 
